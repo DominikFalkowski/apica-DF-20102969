e6d6ffb5ab9efdfabb8ba14772f78f7e7527dfea
Dominik Falkowski <20102969@mail.wit.ie>
2025-01-06 16:04:31 +0000

    ccc

--- react-movies/src/api/watchlist-api.js
+++ react-movies/src/api/watchlist-api.js
@@ -1,22 +1,22 @@
-export const getWatchlist = async (token) => {
+export const getWatchlist = async (ilikecake) => {
     const response = await fetch("http://localhost:8080/api/watchlist", {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${ilikecake}` },
     });
     return await response.json();
   };
   
-  export const addToWatchlist = async (movieId, token) => {
+  export const addToWatchlist = async (movieId, ilikecake) => {
     const response = await fetch(`http://localhost:8080/api/watchlist/${movieId}`, {
       method: "POST",
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${ilikecake}` },
     });
     return await response.json();
   };
   
-  export const removeFromWatchlist = async (movieId, token) => {
+  export const removeFromWatchlist = async (movieId, ilikecake) => {
     const response = await fetch(`http://localhost:8080/api/watchlist/${movieId}`, {
       method: "DELETE",
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${ilikecake}` },
     });
     return await response.json();
   };
